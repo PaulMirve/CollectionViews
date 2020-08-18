41d091fdb8768a858e309a9ef3de62b0466221ea
Paul Miranda <paulmirve@gmail.com>
2020-08-18 12:03:35 -0700

    first commit

---
+++ Diversion/MVVM/Model/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diversion.MVVM.Model
+{
+    public class Animal
+    {
+        public string name { get; set; }
+        public int count { get; set; }
+        public string habitat { get; set; }
+
+        public Animal(string name, int count, string habitat)
+        {
+            this.name = name;
+            this.count = count;
+            this.habitat = habitat;
+        }
+    }
+}
